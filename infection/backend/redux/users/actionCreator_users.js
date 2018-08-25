c28d763e1ec3a236fcec1327a0c0585302fe8530
Matthew Reid <matthew.reid.os1@gmail.com>
2018-08-24 19:24:45 -0500

    Implement socket killer at game end to force all socket ids to leave the game room.

--- infection/backend/redux/users/actionCreator_users.js
+++ infection/backend/redux/users/actionCreator_users.js
@@ -1,4 +1,4 @@
-const { ADD_NEW_USER, ASSIGN_ROLES } = require('./actions_users');
+const { ADD_NEW_USER, ASSIGN_ROLES, RESET_USERS, ASSIGN_OFFICER } = require('./actions_users');
 
 
 const newUser = (username, room, socketID, infiltrator, securityOfficer) => ({
@@ -15,8 +15,18 @@ const assignRoles = (infiltrator) => ({
   infiltrator
 });
 
+const resetUsers = () => ({
+  type: RESET_USERS
+});
+
+const assignSecurityOfficer = (securityOfficer) => ({
+  type: ASSIGN_OFFICER
+});
+
 module.exports = {
   newUser,
-  assignRoles
+  assignRoles,
+  resetUsers,
+  assignSecurityOfficer
 };
 
